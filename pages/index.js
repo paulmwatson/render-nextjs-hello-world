1093a137432b75cb463cd47c58aed3833e08a340
paulmwatson <github.com@paulmwatson.com>
2023-07-25 15:29:03 +0200

    Added Docker, redis, and adjust blueprint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import Link from 'next/link'
-const Index = () => (
+import Link from "next/link";
+import Redis from "ioredis";
+
+const Index = ({data}) => (
   <div>
-    Hello World.{' '}
+    Hello World.{" "}
     <Link href="/about">
       <a>About</a>
     </Link>
+    <p>
+      <b>Redis:</b> {data}
+    </p>
   </div>
-)
+);
 export default Index;
+
+export async function getServerSideProps() {
+  let redis = new Redis(process.env.REDIS_URL);
+  const data = await redis.incr("counter");
+  redis.quit()
+  return { props: { data } }
+}
